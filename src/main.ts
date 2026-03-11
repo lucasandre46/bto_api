78eff41b0b401d9784c14cf4e668a515dd875a6f
BETO <lucasandremiguel46@gmail.com>
2026-03-11 04:58:30 -0300

    atlz_bloqueio_externo

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: 'https://bto-coins.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
