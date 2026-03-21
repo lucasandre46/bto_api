6fe5d416888ceabf1286684622097e8f3c150147
Seu Nome <thiagostopassola@gmail.com>
2026-03-20 21:06:07 -0300

    fix: enable CORS for localhost

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://bto-coins.vercel.app',
+    origin: [
+      'https://bto-coins.vercel.app',
+      'http://localhost:5173',
+      'http://localhost:3000',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
